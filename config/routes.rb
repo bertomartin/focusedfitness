5c227b2ed9ed7d906cedd5d624f840b4b2b4fca1
Roberto <rmartin.martin@gmail.com>
2014-12-20 21:54:40 -0500

    bootstrap and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  #mount Storytime::Engine => "/blog"
   resources :articles
+
+  root to: "articles#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
