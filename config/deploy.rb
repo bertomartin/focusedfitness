ad276e30e1f26a8e019b9d28aefff342a1e39ddb
Roberto <rmartin.martin@gmail.com>
2015-01-04 18:32:54 -0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,7 @@ namespace :deploy do
   # reload nginx so it picks up modified vhosts
   after 'deploy:setup_config', 'nginx:reload'
 
-  #after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
   after :finishing, 'deploy:cleanup'
 
   desc 'Restart application'
